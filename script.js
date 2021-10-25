2fd446e1c4f1025403657fa5ca1350b4277effea
ManupaDev <manupadevsuite@gmail.com>
2021-10-25 17:52:46 +0530

    marker adding

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
+/*
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -32,6 +32,43 @@ if (navigator.geolocation) {
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
+    },
+    function () {
+      alert('Could not get your location!');
+    }
+  );
+}*/
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      //console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const ourcoords = [latitude, longitude];
+      const map = L.map('map').setView(ourcoords, 13); //setView(focused-coordinate, zoomlevel)
+      //L is a global valiable inside the leaflet library, so we can access it.
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(ourcoords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+      
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const {lat, lng} = mapEvent.latlng;
+
+        L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup('Workout')
+        .openPopup();
+      });  
     },
     function () {
       alert('Could not get your location!');
