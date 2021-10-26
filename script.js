0ba3205ce41ec181f2546402c89636b4a7e7fd4e
ManupaDev <manupadevsuite@gmail.com>
2021-10-26 15:12:49 +0530

    reverting

--- script.js
+++ script.js
@@ -60,7 +60,6 @@ class Cycling extends Workout {
 class App {
   #map;
   #mapEvent;
-  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -118,52 +117,7 @@ class App {
   }
 
   _newWorkout(e) {
-    const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
-    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
-
     e.preventDefault(); //prevent page reloading just after rendering the marker when the form is submitted.
-    
-    //Get data from the form.
-    const type = inputType.value;
-    const distance = +inputDistance.value;
-    const duration = +inputDuration.value;
-    const { lat, lng } = this.#mapEvent.latlng; //latitude and longitude valuess are inside the latlng child obj.
-    let workout;
-
-    //If workout is running => create a running object.
-    if (type === 'running') {
-        const cadence = +inputCadence.value;
-        //Check if data is valid.
-        if (!validInputs(distance, duration, cadence) || !allPositive(distance, duration, cadence)) {
-            return alert('Please enter valid inputs.');
-        }
-
-        workout = new Running([lat, lng], distance, duration, cadence);
-        
-    }
-
-    //If workout is cycling  => create a cycling object.
-    if (type === 'cycling') {
-        const elevation = +inputElevation.value;
-        //Check if data is valid.
-        if (!validInputs(distance, duration, elevation) || !allPositive(distance, duration)) {
-            return alert('Please enter valid inputs.');
-        }
-
-        workout = new Cycling([lat, lng], distance, duration, elevation);
-        
-    }
-
-    //add the new object to the workout array.
-    this.#workouts.push(workout);
-
-    //Render workout on map as a marker.
-
-    //Render workout on sidebar list.
-
-    //hide form + clear input fields.
-    
-
 
     //Clear input fields.
     inputDistance.value =
@@ -174,7 +128,7 @@ class App {
 
     //Display the marker.
     console.log(this.#mapEvent); //Returns the event object.
-    
+    const { lat, lng } = this.#mapEvent.latlng; //latitude and longitude valuess are inside the latlng child obj.
     //adding a marker.
     L.marker([lat, lng])
       .addTo(this.#map)
@@ -183,7 +137,7 @@ class App {
         minWidth: 100,
         autoClose: false,
         closeOnClick: false,
-        className: `${type}-popup`,
+        className: 'running-popup',
       })
       .setPopupContent('Workout')
       .openPopup();
@@ -209,7 +163,6 @@ if (navigator.geolocation) {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-
       L.marker(ourcoords)
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
@@ -230,7 +183,6 @@ if (navigator.geolocation) {
       const { longitude } = position.coords;
       console.log(latitude, longitude);
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
-
       const ourcoords = [latitude, longitude];
       const map = L.map('map').setView(ourcoords, 13); //setView(focused-coordinate, zoomlevel)
       //L is a global valiable inside the leaflet library, so we can access it.
@@ -238,7 +190,6 @@ if (navigator.geolocation) {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-
       L.marker(ourcoords)
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
@@ -247,13 +198,11 @@ if (navigator.geolocation) {
       map.on('click', function (mapEvent) {
         console.log(mapEvent);//Returns the event object.
         const {lat, lng} = mapEvent.latlng; //latitude and longitude valuess are inside the latlng child obj.
-
         //adding a marker.
         L.marker([lat, lng])
         .addTo(map)
         .bindPopup('Workout')
         .openPopup();
-
         // CUSTOMIZING 
         //adding a marker.
         //.marker([lat, lng], {<option-object-properties})
@@ -277,7 +226,6 @@ if (navigator.geolocation) {
       const { longitude } = position.coords;
       console.log(latitude, longitude);
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
-
       const ourcoords = [latitude, longitude];
       map = L.map('map').setView(ourcoords, 13); //setView(focused-coordinate, zoomlevel)
       //L is a global valiable inside the leaflet library, so we can access it.
@@ -285,8 +233,6 @@ if (navigator.geolocation) {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-
-
       map.on('click', function (mapE) {//Handling clicks on map.
         form.classList.remove('hidden');
         inputDistance.focus();
@@ -298,7 +244,6 @@ if (navigator.geolocation) {
     }
   );
 }
-
 form.addEventListener('submit', function(e){
     e.preventDefault();//prevent page reloading just after rendering the marker when the form is submitted.
     
@@ -311,7 +256,6 @@ form.addEventListener('submit', function(e){
     //adding a marker.
     L.marker([lat, lng]).addTo(map).bindPopup({maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: "running-popup"}).setPopupContent('Workout').openPopup();
 });
-
 inputType.addEventListener('change', function() {
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
